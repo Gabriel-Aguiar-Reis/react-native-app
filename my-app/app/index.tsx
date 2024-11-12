e9fa98501ac8cbf379d6b7ec17d48f941a9d4e35
Gabriel Aguiar <lugafeagre@gmail.com>
2024-11-12 01:39:32 -0300

    add a test avatar

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import '@/global.css'
+import { Avatar, AvatarImage } from '@/components/Avatar'
 
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white">Hello World!</Text>
+      <Avatar className="h-20 w-20">
+        <AvatarImage
+          source={{ uri: 'https://github.com/Gabriel-Aguiar-Reis.png' }}
+        />
+      </Avatar>
+      <Text className="text-white font-bold text-2xl mt-4">
+        Hello Native World!
+      </Text>
     </View>
   )
 }
